04ee2b07461f7ef89da05ca5e5942799c8f8d160
Rima Jean <rima.jean@gmail.com>
2018-02-22 14:52:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort { |x, y| x.size <=> y.size}
+end
+
+def swap_elements(array)
+  array.sort { |a, b| a[1] <=> b[2]}
+end
+
+def reverse_array(array)
+  array.reverse
+end
 
